ec022657ea5ce13659eeb58318cc01703c82cf7b
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-04-09 14:10:21 +0400

    feat: add new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,12 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "consistent-return": 0,
+        "no-useless-return": 0,
+        "prefer-const": 0,
         "linebreak-style": 0,
         "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    }
+        "quotes": ["error", "double"],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }]
+    },
 };
